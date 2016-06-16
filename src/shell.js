38eae8e4973338ece20937f384e9c246f4d5b1ac
Olena Deering <olena.ivashyna@gmail.com>
2016-06-16 15:35:46 -0600

    add two routes routes and views for them

--- src/shell.js
+++ src/shell.js
@@ -4,7 +4,24 @@ export class Shell {
         this.router = router;
         config.title = "Olena's App";
         config.map([
-            {route: ['', 'events'], moduleId: 'events'} // default route
+            {
+                route: ['', 'events'],
+                moduleId: 'events/events',
+                name: 'Events',
+                title: 'Events',
+                nav: true}, // default route
+            {
+                route: 'jobs',
+                moduleId: 'jobs/jobs',
+                title: 'Jobs',
+                nav: true
+            },
+            {
+                route: 'discussion',
+                moduleId: 'discussion/discussion',
+                title: 'Discussion',
+                nav: true
+            }
         ]);
     }
 }
