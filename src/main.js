16ce3968847f1bdbd94448dbcf8bfc047da22db4
Olena Deering <olena.ivashyna@gmail.com>
2016-06-16 14:40:48 -0600

    refactor main.js

--- src/main.js
+++ src/main.js
@@ -1,11 +1,10 @@
 import 'bootstrap';
-import {LogManager} from 'aurelia-framework';
-import {ConsoleAppender} from 'aurelia-logging-console';
-
-LogManager.addAppender(new ConsoleAppender());
-LogManager.setLevel(LogManager.logLevel.debug);
 
 export function configure(aurelia) {
-    aurelia.use.standardConfiguration();
+    aurelia.use
+        .standardConfiguration()
+        .developmentLogging();
+        //.globalResources('./lib/d3.js');
+
     aurelia.start().then(app => app.setRoot('shell'));
 }
