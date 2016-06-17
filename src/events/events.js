dbe0a52bc59b5e46a02bc1f20e5b2c9702019368
Olena Deering <olena.ivashyna@gmail.com>
2016-06-16 21:50:16 -0600

    use router.navigate

--- src/events/events.js
+++ src/events/events.js
@@ -5,11 +5,16 @@ import {DataRepository} from 'services/dataRepository';
 @inject(DataRepository, Router)
 export class Events {
     constructor(dataRepository, router) {
+        this.router = router;
         dataRepository.getEvents().then(results => {
             this.events = results;
             this.events.forEach(item => {
-                item.detailUrl = router.generate('eventDetail', {eventId: item.id});
+                item.detailUrl = this.router.generate('eventDetail', {eventId: item.id});
             });
         });
+    }
+
+    goToDiscussion() {
+        this.router.navigate('#/discussion');
     }
 }
