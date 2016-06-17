496a8ad9b1d2ef2ab8dd6ce587daf3003950dc86
Olena Deering <olena.ivashyna@gmail.com>
2016-06-17 11:43:19 -0600

    add canDeactivate method to the discussion view

--- src/discussion/discussion.js
+++ src/discussion/discussion.js
@@ -1,1 +1,38 @@
-export class Discussion {}
+function getDiscussionInput() {
+    // fake data access
+    return '';
+}
+
+function cloneObject(obj) {
+    return JSON.parse(JSON.stringify(obj));
+}
+export class Discussion {
+    constructor() {
+        this.discussionInput = '';
+        this.originalInput = '';
+        this.description = 'Discussion input:';
+        this.warn = 'You have unsaved discussion data, do you want to leave this page?';
+    }
+
+    activate() {
+        this.discussionInput = getDiscussionInput();
+        this.originalInput = cloneObject(this.discussionInput);
+    }
+
+    save() {
+        // simulate save
+        this.originalInput = cloneObject(this.discussionInput);
+    }
+
+    canDeactivate() {
+        if (JSON.stringify(cloneObject(this.discussionInput)) != JSON.stringify(this.originalInput)) {
+            if (confirm(this.warn)) {
+                return true;
+            } else {
+                false;
+            }
+        } else {
+            return true;
+        }
+    }
+}
