e125bd63814dd14206dd4eec2043f4488f7a0753
Olena Deering <olena.ivashyna@gmail.com>
2016-06-17 11:43:47 -0600

    add canActivate method to the Jobs view

--- src/jobs/jobs.js
+++ src/jobs/jobs.js
@@ -1,1 +1,11 @@
-export class Jobs {}
+export class Jobs {
+    canActivate(params, routeCOnfig, navigationInstruction) {
+        var promise = new Promise((resolve, reject) => {
+            setTimeout(_ => {
+                resolve(false);
+            }, 3000);
+        });
+
+        return promise;
+    }
+}
