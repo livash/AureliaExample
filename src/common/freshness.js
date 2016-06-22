2944e7cf407ec80e2d49471a5a0331891d37afff
Olena Deering <olena.ivashyna@gmail.com>
2016-06-22 16:24:09 -0600

    remove console.log

--- src/common/freshness.js
+++ src/common/freshness.js
@@ -1,10 +1,5 @@
 export class FreshnessValueConverter {
     toView(value) {
-        console.log({
-            SSSS: value,
-            date: new Date(),
-            delta: new Date() - value
-        });
         if (Math.floor((new Date() - value) / 1000 ) > 10)
             return 'red';
         if (Math.floor((new Date() - value) / 1000 ) > 5)
