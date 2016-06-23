c1e853b3c3e402c1630ff78c8a1286d99651dc6b
Olena Deering <olena.ivashyna@gmail.com>
2016-06-23 15:41:36 -0600

    html component lyfecycle hooks

--- src/common/nav-bar.js
+++ src/common/nav-bar.js
@@ -3,4 +3,15 @@ import {bindable} from 'aurelia-framework';
 export class NavBar {
     @bindable router;
     @bindable image;
+
+    // life-cycle of a custom element
+    created(view) {}
+
+    bind(bindingContext, overrideContext) {}
+
+    unbind() {}
+
+    attached() {}
+
+    detached() {}
 }
